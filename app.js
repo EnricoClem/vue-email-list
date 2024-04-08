6b8497a9f241d67f77c8eb87ce409f1242c41fe7
EnricoClem <enrico.clem94@gmail.com>
2024-04-08 16:49:45 +0200

    ADD - fetchmail function and print on dom

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const { createApp } = Vue
 
 createApp({
-  data() {
-    return {
-      message: 'Hello Vue!'
+    data() {
+        return {
+            newMail: '',
+            randomMails: [],
+        }
+    },
+
+    methods: {
+        fetchMail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((mail) => {
+                        this.newMail = mail.data.response
+                        this.randomMails.push(this.newMail)
+                    })
+            }    
+        }
+    },
+
+    created() {
+        this.fetchMail()
+        console.log(this.randomMails)
     }
-  }
+
 }).mount('#app')
